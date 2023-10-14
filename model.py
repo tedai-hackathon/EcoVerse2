08d73c954ea76c2a351f28e6711d266e5fa7626d
Harsha Sabbineni <harsha@aimorphic.com>
2023-10-14 14:44:03 -0700

    add openai call using langchain

--- model.py
+++ model.py
@@ -1,3 +1,13 @@
+from langchain.schema import (
+    AIMessage,
+    HumanMessage,
+    SystemMessage
+)
+from langchain.chat_models import ChatOpenAI
+
+chat = ChatOpenAI()
+
 
 def get_area(user_input_string):
-    return 552
+    llm_output = chat([HumanMessage(content=f"You are a helpful AI assistant. User has provided a description of usable area for installing a solar panel. {user_input}. Return the area in sq ft from the description: ")])
+    return float(llm_output)

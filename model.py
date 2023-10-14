4224b6294d21c1ae3bd98f113db99898d4ad0c29
Harsha Sabbineni <harsha@aimorphic.com>
2023-10-14 15:21:22 -0700

    use Google Maps API and Object Detection Model API

--- model.py
+++ model.py
@@ -4,6 +4,7 @@ from langchain.chat_models import ChatOpenAI
 chat = ChatOpenAI()
 
 
+<<<<<<< Updated upstream
 def get_area(user_input_string):
     llm_output = chat(
         [
@@ -13,3 +14,12 @@ def get_area(user_input_string):
         ]
     )
     return float(llm_output)
+=======
+def get_usable_area(user_input_string):
+    llm_output = chat([HumanMessage(content=f"You are a helpful AI assistant. User has provided a textual description of usable area for installing a solar panel. {user_input}. Extract the usable area in sq ft from the description: ")])
+    return float(llm_output)
+
+def get_total_area(user_input_string):
+    llm_output = chat([HumanMessage(content=f"You are a helpful AI assistant. User has provided a textual description of usable area for installing a solar panel. {user_input}. Extract the total area in sq ft from the description: ")])
+    return float(llm_output)
+>>>>>>> Stashed changes

9c79104f506ce31ff3c50c7603ccaa6c49f7c136
BigTava <bigtavadev@gmail.com>
2023-10-14 23:08:22 +0100

    wip: pvgis api call

---
+++ energy.py
@@ -0,0 +1,20 @@
+import requests
+import json
+
+API_URL = "https://re.jrc.ec.europa.eu/api/v5_2/seriescalc?"
+
+
+def create_pvgis_api_url(lat: float, lon: float, peak_power: float):
+    return f"{API_URL}lat={lat}&lon={lon}&peakpower={peak_power}&startyear=2020&endyear=2020&outputformat=json"
+
+
+def get_radiation_data(coordinates, area):
+    url = create_pvgis_api_url(3.5, 3.5, 1)
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = json.loads(response.text)
+        return data
+    else:
+        print(f"Failed to get data. Status code: {response.status_code}")
+        return None

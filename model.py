8b012e8eb519b289b1b0ab81204bb528bec3ccb4
BigTava <bigtavadev@gmail.com>
2023-10-14 23:14:42 +0100

    wip: merge problems

--- model.py
+++ model.py
@@ -1,3 +1,15 @@
+from langchain.schema import AIMessage, HumanMessage, SystemMessage
+from langchain.chat_models import ChatOpenAI
+
+chat = ChatOpenAI()
+
 
 def get_area(user_input_string):
-    return 552
+    llm_output = chat(
+        [
+            HumanMessage(
+                content=f"You are a helpful AI assistant. User has provided a textual description of usable area for installing a solar panel. {user_input}. Extract the area in sq ft from the description: "
+            )
+        ]
+    )
+    return float(llm_output)

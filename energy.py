4a2e280dd1fbd80515c274da7acea58184079f1c
Srajan Gupta <srajangupta@Srajan-Gupta-mbp.dave.com>
2023-10-14 15:56:45 -0700

    integration

--- energy.py
+++ energy.py
@@ -8,8 +8,8 @@ def create_pvgis_api_url(lat: float, lon: float, peak_power: float):
     return f"{API_URL}lat={lat}&lon={lon}&peakpower={peak_power}&startyear=2020&endyear=2020&outputformat=json"
 
 
-def get_radiation_data(coordinates, area):
-    url = create_pvgis_api_url(3.5, 3.5, 1)
+def get_radiation_data(lat, lon, area):
+    url = create_pvgis_api_url(lat=lat, lon=lon, peak_power=1.0),
     response = requests.get(url)
 
     if response.status_code == 200:
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import streamlit as st
 # from dashboard import func
+from model import get_total_area, get_usable_area
+from energy import get_radiation_data
 
 def main():
    st.title("Ecoverse")
@@ -8,6 +10,7 @@ def main():
     # Create a form
    with st.form(key='my_form'):
       address = st.text_input(label='Enter your address', placeholder= '555 California Street, San Francisco')
+      # my total sqft area is 500 and usable sqft area is 350
       description = st.text_area(label='Tell us about your location', placeholder= 'What is the sq ft area, What is the usable area, Near San Marie Square')
 
         # Create a submit button
@@ -19,9 +22,12 @@ def main():
          st.write(f"Your address: {address}")
       if description:
          # extracted sqft information from LLM
-         st.write(f"Information: {description}")
-         
-   
+         # sqft = get_total_area(description)
+         sqft = 100
+         # usqft = get_usable_area(description)
+         radiation_data = get_radiation_data(37.792072824760965, 122.40419151380131, sqft)
+         st.table(data=radiation_data)
+         # st.write(f"Information: {description}")
 
 if __name__ == "__main__":
    main()

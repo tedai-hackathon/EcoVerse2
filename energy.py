1ffe1ffe50b44159962ceeb52e1f83da481596f4
BigTava <bigtavadev@gmail.com>
2023-10-15 02:40:31 +0100

    wip: energy produced every hour

--- energy.py
+++ energy.py
@@ -4,19 +4,19 @@ import json
 API_URL = "https://re.jrc.ec.europa.eu/api/v5_2/seriescalc?"
 
 
-def create_pvgis_api_url(lat: float, lon: float, peak_power: float):
-    return f"{API_URL}lat={lat}&lon={lon}&peakpower={peak_power}&startyear=2020&endyear=2020&pvcalculation=1&outputformat=json&loss=0.14"
+def create_pvgis_api_url(lat: float, lon: float):
+    return f"{API_URL}lat={lat}&lon={lon}&startyear=2020&endyear=2020&outputformat=json"
 
 
 def get_radiation_data(coordinates, area):
-    url = create_pvgis_api_url(40.767, -7.910, 1)
+    url = create_pvgis_api_url(coordinates[0], coordinates[1])
     response = requests.get(url)
 
     if response.status_code == 200:
         data = json.loads(response.text)
         hourly_data = data["outputs"]["hourly"]
 
-        return [{"time": data["time"], "P": data["P"]} for data in hourly_data]
+        return [{"time": data["time"], "Energy (kWh)": data["G(i)"] * area * 0.18 * (1 - 0.14) / 1000} for data in hourly_data]
     else:
         print(f"Failed to get data. Status code: {response.status_code}")
         return None
